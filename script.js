abe695206c455e00976a588445818cfa895f63ff
Nafis Mohammed Jami <nafismohammedjami@gmail.com>
2026-04-20 13:08:25 +0600

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ el.style.display = (el.style.display==="block")?"none":"block";
 function toggleRow(id){
 document.getElementById(id).classList.toggle("hidden");
 }
+/* 🔥 hamburger menu */
+function toggleMenu(){
+document.querySelector(".nav-links").classList.toggle("active");
+}
+
+/* 🔥 dropdown */
+function toggleDrop(id){
+let el=document.getElementById(id);
+el.style.display = (el.style.display==="block") ? "none" : "block";
+}
+
+/* 🔥 table expand */
+function toggleRow(id){
+document.getElementById(id).classList.toggle("hidden");
+}
