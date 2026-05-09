f49c4092e7f80ec087fec9651f425f10ef20c3e3
Nafis Mohammed Jami <nafismohammedjami@gmail.com>
2026-05-09 19:53:39 +0600

    Update index.js

--- index.js
+++ index.js
@@ -43,3 +43,16 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+function toggleLOS(id){
+
+  let content = document.getElementById(id);
+
+  if(content.style.display === "block"){
+    content.style.display = "none";
+  }else{
+    content.style.display = "block";
+  }
+
+}
